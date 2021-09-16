d9c6783498cc48fe1423e8d772164459912237f7
Brendan Palmisano <Brendan.P.Palmisano-1@ou.edu>
2021-09-16 15:10:26 -0500

    completed

---
+++ InClassExamples/WPF_Classes1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string id, first, last, gpa, bursar;
+            id = txtSoonerID.Text;
+            first = txtFirst.Text;
+            last = txtLast.Text;
+            bursar = txtBursar.Text;
+            gpa = txtGPA.Text;
+
+            Student stud = new Student(Convert.ToInt32(id), first, last, Convert.ToDouble(bursar));
+            stud.GPA = Convert.ToDouble(gpa);
+            stud.isOnProbation = chkProbation.IsChecked.Value;
+            //if (probation.ToLower() == "yes")
+            //{
+            //    stud.isOnProbation = true;
+            //}
+            //else
+            //{
+            //    stud.isOnProbation = false;
+            //}
+            lstStudents.Items.Add(stud);
+        }
+
+        private void lstStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Student selectedStudent = (Student) lstStudents.SelectedItem;
+            MessageBox.Show($"{selectedStudent.ToString()} has a bursar balance of {selectedStudent.CheckBalance().ToString("C")}");
+        }
+    }
+}
---
+++ InClassExamples/WPF_Classes1/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes1
+{
+    public class Student
+    {
+        public int SoonerID{get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public bool isOnProbation {get; set;}
+        public double GPA{ get; set; }
+        private double BursarBalance { get; set; }
+
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            isOnProbation = false;
+            GPA = 0;
+            BursarBalance = 0;
+
+        }
+        public Student(int id, string fName, string lName, double bursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            BursarBalance = bursarBalance;
+            isOnProbation = false;
+            GPA = 0;
+
+        }
+        public void MakePayment(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new Exception("Amount must be positive");
+                //Console.WriteLine("Amount must be positive");
+            }
+            BursarBalance -= amount;
+
+        }
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName}, {LastName} ({SoonerID})";
+        }
+    }
+}
